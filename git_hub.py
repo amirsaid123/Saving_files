086dd6aa76769d05138c1dfa6cda8b1fa4786e6c
Пользователь <saidalosamigjonov071.com>
2024-12-17 20:54:37 +0500

    first commit

---
+++ git_hub.py
@@ -0,0 +1,12 @@
+"""
+echo "# Saving_files" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/amirsaid123/Saving_files.git
+git push -u origin main
+git remote add origin https://github.com/amirsaid123/Saving_files.git
+git branch -M main
+git push -u origin main
+"""
